eda78786074c46062ea4d474c8d8747cae08a50d
Pepe <jospalau@jospalau-laptop.casa>
2024-04-14 08:02:49 +0300

    When in history and when books opened, don't allow to set reading book in tbr

--- frontend/apps/filemanager/filemanagerutil.lua
+++ frontend/apps/filemanager/filemanagerutil.lua
@@ -141,12 +141,14 @@ function filemanagerutil.statusToString(status)
         new       = _("Unread"),
         reading   = _("Reading"),
         abandoned = _("On hold"),
-        complete  = _("Finished"),
+
+     complete  = _("Finished"),
         tbr  = _("TBR"),
     }
 
     return status_to_text[status]
 end
+
 
 -- Generate all book status file dialog buttons in a row
 function filemanagerutil.genStatusButtonsRow(doc_settings_or_file, caller_callback)
@@ -164,14 +166,14 @@ function filemanagerutil.genStatusButtonsRow(doc_settings_or_file, caller_callba
 
         local enabled = false
 
-        local ui = require("apps/filemanager/filemanager").instance
+        local ui = require("apps/reader/readerui").instance
         -- If we are not in the File Manager (we are in an opened book)
-        -- don't allow to change books to tbr
+        -- don't allow to change the current reading book to the tbr
         -- because opened tbr books status is moved to reading when opening them (readerui.lua)
-        if to_status == "tbr" and not ui then
+        if to_status == "tbr" and ui and ui.document and ui.document.file and file == ui.document.file then
             enabled = false
         else
-            enabled = status ~= to_status
+             enabled = status ~= to_status
         end
         return {
             text = filemanagerutil.statusToString(to_status) .. (status == to_status and "  ✓" or ""),
