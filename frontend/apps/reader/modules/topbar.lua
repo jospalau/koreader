486f746aa30268a37ecdf3da9ab46452b51b5fef
Pepe <jospalau@gmail.com>
2024-07-15 19:30:42 +0200

    Update topbar margin and front light markers when topbar is not visible

--- frontend/apps/reader/modules/topbar.lua
+++ frontend/apps/reader/modules/topbar.lua
@@ -966,6 +966,31 @@ function TopBar:toggleBar()
         self.progress_chapter_bar.width = 0
         self.times_text_text = ""
         self.time_battery_text_text = ""
+        local configurable = self.ui.document.configurable
+        local powerd = Device:getPowerDevice()
+        if self.last_frontlight ~= nil then
+            self.frontlight = self.last_frontlight
+            self.last_frontlight = nil
+        else
+            if powerd:isFrontlightOn() then
+                self.frontlight = " ☼"
+            else
+                self.frontlight = ""
+            end
+        end
+        if Device:isAndroid() then
+            if configurable.h_page_margins[1] == 20 and configurable.t_page_margin == 9 + 6 and configurable.h_page_margins[2] == 20 and configurable.b_page_margin == 15 then
+                self.test_light:setText(" ● " .. self.frontlight)
+            else
+                self.test_light:setText(" ○ " .. self.frontlight)
+            end
+        else
+            if configurable.h_page_margins[1] == 15 and configurable.t_page_margin == 9 + 6 and configurable.h_page_margins[2] == 15 and configurable.b_page_margin == 15 then
+                self.test_light:setText(" ● " .. self.frontlight)
+            else
+                self.test_light:setText(" ○ " .. self.frontlight)
+            end
+        end
     end
 end
 
