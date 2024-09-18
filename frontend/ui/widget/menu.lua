58a84934c44e41982e22eb967d33d0bac83da515
Pepe <jospalau@jospalau-laptop.casa>
2024-09-18 07:50:56 +0300

    Flash in menu items look glitchy in the new Kobo BW. Use different waveform for this device in this other case

--- frontend/ui/widget/menu.lua
+++ frontend/ui/widget/menu.lua
@@ -606,7 +606,11 @@ function MenuItem:onHoldSelect(arg, ges)
         --
         self[1].invert = true
         UIManager:widgetInvert(self[1], self[1].dimen.x, self[1].dimen.y)
-        UIManager:setDirty(nil, "fast", self[1].dimen)
+        if Device.model == "Kobo_spaBW" then -- fast is a bit glitchy in Kobo BW
+            UIManager:setDirty(nil, "ui", self[1].dimen)
+        else
+            UIManager:setDirty(nil, "fast", self[1].dimen)
+        end
 
         UIManager:forceRePaint()
         UIManager:yieldToEPDC()
