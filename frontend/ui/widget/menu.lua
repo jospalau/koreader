a306cf1f6b664fe1b8e6dd2c5e7705989eba6996
Pepe <jospalau@jospalau-laptop.casa>
2025-03-07 17:33:24 +0300

    Close the history when opening a book from the history. The widget remains in the widget list and some glitches occur next time when opening the document

--- frontend/ui/widget/menu.lua
+++ frontend/ui/widget/menu.lua
@@ -1513,6 +1513,9 @@ function Menu:onMenuSelect(item)
                 text = text,
                 choice1_text = _("Yes"),
                 choice1_callback = function()
+                    if self.ui.history.booklist_menu then
+                        UIManager:close(self.ui.history.booklist_menu)
+                    end
                     self:onMenuChoice(item)
                 end,
                 choice2_text = _("Do not open it"),
