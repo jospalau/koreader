b1c3051665d388bfb90ead2d957ee3bf975ee740
Pepe <jospalau@jospalau-laptop.casa>
2025-10-04 14:17:57 +0300

    Revert "Fix book map not showing correct read pages and bookmarks when using reference pages. This happens since the reference total number of pages is used for books in the book table of the statistics db"
    This reverts commit b30a6b19c4c07705523b6199ebcb37fa6b572868.

--- frontend/apps/reader/modules/readerbookmark.lua
+++ frontend/apps/reader/modules/readerbookmark.lua
@@ -618,16 +618,7 @@ end
 function ReaderBookmark:getBookmarkedPages()
     local pages = {}
     for _, bm in ipairs(self.ui.annotation.annotations) do
-        local page
-        -- Take into account reference page numbers since
-        -- the reference total number of pages is used
-        -- for books in the book table of the statistics db
-        -- if use reference page numbers option is enabled
-        if self.ui.pagemap:wantsPageLabels() then
-            page = tonumber(bm.pageref)
-        else
-            page = self:getBookmarkPageNumber(bm)
-        end
+        local page = self:getBookmarkPageNumber(bm)
         local btype = self.getBookmarkType(bm)
         if not pages[page] then
             pages[page] = {}
