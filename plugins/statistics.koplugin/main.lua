19a240cf58a3e3cd1fe21fda3ecbfd88c40274f3
Pepe <jospalau@jospalau-laptop.casa>
2025-10-04 14:18:57 +0300

    Fix book map not showing correct read pages and bookmarks when using reference pages. This happens since the reference total number of pages is used for books in the book table of the statistics db

--- plugins/statistics.koplugin/main.lua
+++ plugins/statistics.koplugin/main.lua
@@ -3813,6 +3813,42 @@ function ReaderStatistics:getCurrentBookReadPages()
     local res, nb = stmt:reset():bind(self.settings.max_sec, self.id_curr_book):resultset("i")
     stmt:close()
     conn:close()
+
+    if self.use_pagemap_for_stats then
+        local total_screen_pages = self.document:getPageCount()
+        local sql_stmt = [[
+            SELECT
+                page,
+                min(sum(duration), ?) AS durations,
+                strftime("%s", "now") - max(start_time) AS delay
+            FROM (
+                -- Not sure why I have to floor() it here, while not needed in the VIEW page_stat...
+                SELECT floor(first_page + idx - 1) AS page, start_time, duration / (last_page - first_page + 1) AS duration
+                FROM (
+                    SELECT start_time, duration,
+                        -- First page_number for this page after rescaling single row
+                        ((page - 1) * ?) / total_pages + 1 AS first_page,
+                        -- Last page_number for this page after rescaling single row
+                        max(((page - 1) * ?) / total_pages + 1, (page * ?) / total_pages) AS last_page,
+                        idx
+                    FROM page_stat_data
+                    -- Duplicate rows for multiple pages as needed (as a result of rescaling)
+                    JOIN (SELECT number as idx FROM numbers) AS N ON idx <= (last_page - first_page + 1)
+                    WHERE id_book = ?
+                )
+            )
+            GROUP BY page
+            ORDER BY page;
+        ]]
+
+        conn = SQ3.open(db_location)
+        stmt = conn:prepare(sql_stmt)
+        res, nb = stmt:reset():bind(self.settings.max_sec, total_screen_pages, total_screen_pages, total_screen_pages, self.id_curr_book):resultset("i")
+        -- logger.warn(res)
+        stmt:close()
+        conn:close()
+    end
+
     local read_pages = {}
     local max_duration = 0
     for i=1, nb do
