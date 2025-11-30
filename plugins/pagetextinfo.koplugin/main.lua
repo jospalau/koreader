7acef9a6fa883bab00fd65a8a2f7dc635e133b02
Pepe <jospalau@jospalau-laptop.casa>
2025-11-30 23:38:04 +0300

    Little fix for grid in list view mode. Remove the border between items

--- plugins/pagetextinfo.koplugin/main.lua
+++ plugins/pagetextinfo.koplugin/main.lua
@@ -5545,8 +5545,12 @@ function PageTextInfo:getSubfolderCoverGrid(filepath, max_w, max_h, mosaic)
 
     local function get_stack_grid_size(max_w, max_h)
         local border = Size.border.thin
-        -- local gap    = Size.padding.small
-        local gap    = ((mosaic and self.settings:isTrue("enable_extra_tweaks_mosaic_view") or (not mosaic and self.settings:isTrue("enable_extra_tweaks_list_menu_view")))) and 0 or Size.padding.small
+        local gap    = Size.padding.small
+        if mosaic and self.settings:isTrue("enable_extra_tweaks_mosaic_view") then
+            gap = 0
+        elseif not mosaic and self.settings:isTrue("enable_extra_tweaks_list_menu_view") then
+            gap = -2*border -- -2*border compensantes local border_size = Size.border.thin in listmenu.lua
+        end
         local max_img_w = math.ceil((max_w - (border * 4) - gap) / 2)
         local max_img_h = math.ceil((max_h - (border * 4) - gap) / 2)
         if max_img_w < 10 then max_img_w = max_w * 0.8 end
