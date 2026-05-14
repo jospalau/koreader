97b2baca0316d4edd99412a421c9d4dd9b60cc8f
Pepe <jospalau@jospalau-laptop.casa>
2026-05-14 11:21:51 +0300

    Remove Simple UI plugin

--- plugins/simpleui.koplugin/_meta.lua
+++
@@ -1,9 +0,0 @@
-
-
-return {
-    name        = "simpleui",
-    fullname    = "Simple UI",
-    description = [[A simple UI for KOReader]],
-    version     = "1.4.0",
-    author      = "Doctor Hetfield",
-}
