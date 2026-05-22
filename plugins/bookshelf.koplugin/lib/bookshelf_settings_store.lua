afb9b7c609ae54b7ff38cdbc2060c357a0cb12d8
Pepe <jospalau@gmail.com>
2026-05-23 01:44:04 +0200

    Update Bookshelf plugin to v2.2.5

--- plugins/bookshelf.koplugin/lib/bookshelf_settings_store.lua
+++ plugins/bookshelf.koplugin/lib/bookshelf_settings_store.lua
@@ -41,7 +41,9 @@ local LEGACY_KEYS = {
     -- UX toggles
     "show_close_msg", "show_series_num",
     -- Cover-progress indicator colours / toggles
-    "progress_fill", "progress_track",
+    "progress_fill", "progress_track", "bookmark_color",
+    "badge_fg", "badge_bg",
+    "folder_overlay_bg", "folder_overlay_fg",
     "progress_badge_enabled", "progress_bar_enabled",
     "progress_bookmark_enabled", "progress_enabled",
     -- Legacy v1.1 single-key sort flags (kept for back-compat read path)
