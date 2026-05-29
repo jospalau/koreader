f01305405f71316ba70603ab0ae7ec6a20d90db2
Pepe <jospalau@gmail.com>
2026-05-29 10:57:31 +0200

    Add @quanganhdo reading stats popup patch

--- reader.lua
+++ reader.lua
@@ -222,6 +222,7 @@ if G_reader_settings:isTrue("apply_extra_patches") then
     -- local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/quick-settings.lua")
     local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/dogear-custom.lua")
     local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/top_reading_sessions.lua")
+    local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/reading-stats-popup.lua")
 end
 
 -- Inform once about color rendering on newly supported devices
