8646bef47fc513182fa8d5610a172307a4057d95
Pepe <jospalau@jospalau-laptop.casa>
2026-07-22 13:14:52 +0300

    Disable animation-Koreader patch

--- reader.lua
+++ reader.lua
@@ -227,7 +227,7 @@ if G_reader_settings:isTrue("apply_extra_patches") then
     local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/typography_popup.lua")
     local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/book_info_popup.lua")
     local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/bookloadcover-plus.lua")
-    local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/animation-Koreader.lua")
+    -- local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/animation-Koreader.lua")
     local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/bookshelf-overlay.lua")
     local ok, err = pcall(dofile, "plugins/pagetextinfo.koplugin/touchmenu-icon-warmup.lua")
 end
