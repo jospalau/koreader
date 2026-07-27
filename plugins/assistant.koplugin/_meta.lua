bc55c00f223ff8dd249d996fc3ad71c731130981
Pepe <jospalau@gmail.com>
2026-07-27 22:07:27 +0200

    Remove Assistant plugin

--- plugins/assistant.koplugin/_meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("assistant_gettext")
-return {
-    name = "assistant",
-    fullname = _("AI Assistant"),
-    description = _("💡 Enhance your KOReader experience with AI"),
-    version = "1.10",
-}
